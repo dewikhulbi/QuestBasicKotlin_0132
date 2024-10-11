461ecef9756aa83e90e60b690abb5960fa543b33
dewikhulbi <dewikhlb20@gmail.com>
2024-10-11 21:14:50 +0700

    // Lambda Functions menggunakan tanda panah -> untuk memisahkan parameter dan body fungsi

---
+++ app/src/main/java/com/example/myapplication/LambdaFunction.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+//Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+// HELLO
+}

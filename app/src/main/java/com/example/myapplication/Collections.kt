5be8637537d013baf07e397595527d1f99a2da5a
dewikhulbi <dewikhlb20@gmail.com>
2024-10-11 23:05:07 +0700

    // List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan // List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah // List Read-Only menggunakan listOf // List Mutable menggunakan mutableListOf

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication
 
-// List adalah Kumpulan data yang disusun secara berurutan dari
+// List adalah Kumpulan data yang disusun secara berurutan
 // List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
 // List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
 // List Read-Only menggunakan listOf

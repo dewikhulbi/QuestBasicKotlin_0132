dbed57e75f660dcd92fa827824c458eb1a7cde42
dewikhulbi <dewikhlb20@gmail.com>
2024-10-11 23:06:21 +0700

    // Map adalah Kumpulan data yang disusun dalam pasangan key-value // Map bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu // Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah // Map Read-Only menggunakan mapOf // Map Mutable menggunakan mutableMapOf

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -1,68 +1,6 @@
 package com.example.myapplication
 
-// List adalah Kumpulan data yang disusun secara berurutan
-// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
-// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
-// List Read-Only menggunakan listOf
-// List Mutable menggunakan mutableListOf
 
-fun ContohList() {
-    println("=== List ===")
-// List Read-Only
-    val readOnlyAbjad = listOf("A", "B", "C")
-    println(readOnlyAbjad)
-
-// List Mutable
-    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
-    println(shape)
-
-// Menambahkan data ke dalam list mutable
-    shape.add("Circle")
-    println(shape)
-
-// Menghapus data dari list Mutable
-    shape.remove("Triangle")
-    println(shape)
-
-// Mengubah data di dalam list Mutable
-    shape[0] = "Oval"
-    println(shape)
-
-// List Read-Only
-    val shapesLocked: List<String> = shape
-    println(shapesLocked)
-}
-
-// Set adalah Kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi
-// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
-// Set bersifat mutable dan read-only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
-// Set Read-Only menggunakan setOf
-// Set Mutable menggunakan mutableSetOf
-
-fun ContohSet() {
-    println()
-    println("=== Set ===")
-
-    // Set Read-Only
-    val readOnlyAbjad = setOf("A", "B", "C")
-    println(readOnlyAbjad)
-
-    // Set Mutable
-    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
-    println(shape)
-
-    // Menambahkan data ke dalam Set Mutable
-    shape.add("Rectangle")
-    println(shape)
-
-    // Menghapus data dari Set Mutable
-    shape.remove("Circle")
-    println(shape)
-
-    // Set Read-Only
-    val shapesLocked: Set<String> = shape
-    println(shapesLocked)
-}
 
 // Map adalah Kumpulan data yang disusun dalam pasangan key-value
 // Map bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
